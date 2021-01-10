7cd829dd784bf9fcd5bc31c544d92bc88f4b6e97
Michael <michael.mulligan@stu.fortthomas.kyschools.us>
2021-01-10 12:10:42 -0500

    Update cogs.py

--- cogs.py
+++ cogs.py
@@ -392,7 +392,7 @@ class Default(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send("Pong")
+        await ctx.send("Pong 2")
 
     @commands.command()
     async def test(self, ctx):
